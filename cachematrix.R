70e15cf32d6b833e970335ee2ea05e92b202d831
vpetrovic28 <vpetrovicv@netscape.net>
2015-04-22 13:10:48 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,35 @@
 ## Cache the inverse of a matrix, if this already done return the value 
+
 ## Function makeCacheMatrix creates a  matrix object in cache
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y){
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setSolve <- function(solve) m <<- solve
-    getSolve <- function() m
-    list(set = set, get = get,
-        setSolve = setSolve,
-        getSolve = getSolve)
+	mm <- NULL
+	set <- function(y){
+		x <<- y
+		mm <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) mm <<- inverse
+	getinverse <- function() mm 
+	list(set = set, get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
 
 ## computes the inverse of the matrix returned by makeCacheMatrix if the previously is not compute or not changed
- ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-    m <- x$getSolve()
-    if (!is.null(m)){
-        mb <- makeCacheMatrix()
-        if (mb == x){
-            return (m)
-        }
-    } 
-    m <- makeCacheMatrix(x)
-    x$setSolve(m)
-    return (m)
+	m <- x$getinverse()
+	if (!is.null(m)){
+		mb <- x$get()
+		if (mb == x){
+			return (m)
+		}
+	} 
+		mb <- x$get()
+	mi <- solve(mb)
+	x$setinverse(mi)
+	return (mi)
 }
